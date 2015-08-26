26906256e2f3afb8c481d711d658a58f3b68006e
yumashev <a.yumashev@gmail.com>
2015-08-26 16:58:17 +0500

    write function

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: CallBackHunter for WordPress
-Version: 0.1
-Plugin URI: ${TM_PLUGIN_BASE}
+Version: 20150825
+Plugin URI: https://github.com/systemo-biz/callbackhunter-com-wp
 Description: Integrate CallBackHunter in WordPress
 Author: Systemo
 Author URI: http://systemo.biz/
@@ -11,5 +11,31 @@ Author URI: http://systemo.biz/
 add_action( 'wp_footer', $function_to_add = 'wp_footer_add_callbackhunter', $priority = 10);
 
 function wp_footer_add_callbackhunter(){
-  echo '<div class="testc_cbg"></div>';
+  $cbh = get_option( 'cbh_script_field' );
+  if($cbh) echo $cbh;
+}
+
+add_action( 'admin_init', function(){
+	add_settings_section(
+		'cbh_script_section',
+		'Вставьте тут скрипт от CallBackHunter',
+		'',
+		'discussion'
+	);
+
+  register_setting( 'discussion', 'cbh_script_field');
+
+	add_settings_field(
+		'cbh_script_field',
+		'Введите скрипт от CallBackHunter',
+		'cbh_script_field_callback',
+		'discussion',
+		'cbh_script_section'
+	);
+
+});
+function cbh_script_field_callback() {
+  ?>
+	 <input name="cbh_script_field" type="text" value="<?php echo get_option( 'cbh_script_field' ) ?>">
+  <?php
 }
